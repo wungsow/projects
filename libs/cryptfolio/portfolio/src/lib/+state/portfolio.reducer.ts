301ee2695005315dddc8d74464faf7118f9e34a9
Luke McMenemy <luke.mcmenemy@civica.com.au>
2018-06-04 15:43:33 +1000

    Added selector to build portfolio data

--- libs/cryptfolio/portfolio/src/lib/+state/portfolio.reducer.ts
+++ libs/cryptfolio/portfolio/src/lib/+state/portfolio.reducer.ts
@@ -1,7 +1,8 @@
-import { CoinsState } from './../../../../coins/src/lib/+state/coins.reducer';
-import { Coin } from '@projects/cryptfolio/coins/src/lib/+state/coins.reducer';
-import { Action } from '@ngrx/store';
+import { TickersData } from './../../../../tickers/src/lib/+state/tickers.reducer';
+import { Coin, CoinsState } from '@projects/cryptfolio/coins/src/lib/+state/coins.reducer';
+import { Action, createSelector } from '@ngrx/store';
 import { PortfolioActions, PortfolioActionTypes } from './portfolio.actions';
+import { TickersState, Ticker } from '@projects/cryptfolio/tickers/src/lib/+state/tickers.reducer';
 
 /**
  * Interface for the 'Portfolio' data used in
@@ -23,8 +24,12 @@ export interface Purchase {
  * Interface to the part of the Store containing PortfolioState
  * and other information related to PortfolioData.
  */
-export interface PortfolioState extends CoinsState {
+export interface PortfolioState extends CoinsState, TickersState {
   readonly portfolio: PortfolioData;
+}
+
+export interface PortfolioEntry extends Purchase {
+  coinData: Ticker;
 }
 
 export const initialState: PortfolioData = {};
@@ -41,4 +46,11 @@ export function portfolioReducer(
     default:
       return state;
   }
+}
+
+export const portfolioEntries = createSelector([(state: PortfolioState) => state.portfolio, (state: PortfolioState) => state.tickers], getPortfolioEntries);
+
+function getPortfolioEntries(portfolio: PortfolioData, tickers: TickersData): PortfolioEntry[] {
+  const entries: Purchase[] = portfolio ? Object.values(portfolio) : [];
+  return entries.map(portfolioEntry => ({ ...portfolioEntry, coinData: tickers && tickers[portfolioEntry.coinId] }));
 }
