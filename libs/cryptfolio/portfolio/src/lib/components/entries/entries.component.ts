d3dacc15bedd4607ba3ced808e4329e87a930abd
Luke McMenemy <luke.mcmenemy@civica.com.au>
2018-06-05 14:50:44 +1000

    Added columns to portfolio table

--- libs/cryptfolio/portfolio/src/lib/components/entries/entries.component.ts
+++ libs/cryptfolio/portfolio/src/lib/components/entries/entries.component.ts
@@ -1,13 +1,20 @@
 import { PortfolioEntry } from './../../+state/portfolio.reducer';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'pflo-entries',
   templateUrl: './entries.component.html',
-  styleUrls: ['./entries.component.scss']
+  styleUrls: ['./entries.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EntriesComponent {
   @Input() loading = true;
-  @Input() entries: PortfolioEntry[]
+  @Input() entries: PortfolioEntry[];
   constructor() { }
+
+  getChange({ price, quote }: PortfolioEntry): number {
+    const currentPrice = quote && quote.price;
+
+    return price && currentPrice && Math.round(100 * (currentPrice - price) / price);
+  }
 }
